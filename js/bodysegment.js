942a25640e8ae2dd8defb24c800c7fc0ecdde91c
Kev Adsett <kev.adsett@gmail.com>
2013-10-02 20:11:37 +0100

    Fixed conflicts in bodysegment.js

--- js/bodysegment.js
+++ js/bodysegment.js
@@ -1,94 +1,3 @@
-<<<<<<< HEAD
-function SegmentModel(index, size, baseColour, brightnessVariation) {
-    this.index = index;
-    this.size = size;
-    this.colour = baseColour;
-    this.brightnessVariation = brightnessVariation;
-    this.moveDecisions = [];
-    this.position = new Vector(0, 0);
-    this.velocity = new Vector(0, 0);
-}
-
-function SegmentController(model, parent) {
-    this.model = model;
-    this.parentModel = parent;
-    this.setRandomColour();
-    this.inheritParentPosition();
-    this.view = new SegmentView(model, game.context);
-    game.on('update', this.update, this);
-}
-
-SegmentController.prototype = {
-    
-    advance: function() {
-        this.model.position = this.model.position.add(this.model.velocity);
-        if(this.nextDecisionReached()) {
-            this.model.velocity = this.model.moveDecisions.shift().velocity;
-        }
-    },
-    
-    nextDecisionReached: function() {
-        var decisionPosition = this.model.moveDecisions[0] ? this.model.moveDecisions[0].position : undefined,
-            position = this.model.position,
-            velocity = this.model.velocity;
-        if(decisionPosition == undefined) return false;
-        if(this.model.position.equalTo(this.model.moveDecisions[0].position)) {
-            return true;
-        } else {
-            if(this.model.velocity.x < 0) {
-                if(this.model.velocity.y < 0) {
-                    // if position x < move decision x && move decision x < position x + velocity x
-                    if(position.x < decisionPosition.x
-                       && position.y < decisionPosition.y){
-                        return true;
-                    }
-                } else {
-                    if(position.x < decisionPosition.x
-                       && position.y > decisionPosition.y){
-                        return true;
-                    }
-                }
-            } else {
-                if(this.model.velocity.y < 0) {
-                    if(position.x > decisionPosition.x
-                       && position.y < decisionPosition.y){
-                        return true;
-                    }
-                } else {
-                    if(position.x > decisionPosition.x
-                       && position.y > decisionPosition.y){
-                        return true;
-                    }
-                }
-            }            
-        }
-        return false;
-    },
-    
-    inheritParentPosition: function() {
-        var hyp = new Vector(this.model.size/2, this.model.size/2).mag();
-        this.model.position = this.parentModel.velocity.getPositionBehind(hyp).add(this.parentModel.position);
-        this.model.velocity = this.parentModel.velocity.copy();
-    },
-    
-    onMoveDecisionMade: function(data) {
-        this.model.moveDecisions.push({position:data.pos, velocity:data.vel});
-    },
-    
-    setRandomColour: function() {
-        this.model.brightnessVariation = mapValue(this.model.brightnessVariation, 0, 1, 0, (Math.random() * 2) - 1);
-        var brightnessModifier = parseInt(mapValue(this.model.brightnessVariation, -1, 1, -100, 100)),
-            newTailColour = brightenColour(this.model.colour, brightnessModifier);
-        this.model.colour = rgbObjToHexColourString(newTailColour);
-    },
-    
-    update: function() {
-        this.advance();
-    }
-}
-
-SegmentView = function(model, context) {
-=======
 /*
 * Model
 */
@@ -107,7 +16,6 @@ function SegmentModel(size, baseColour, stripy, startPosition) {
 */
 SegmentView = function(model, context) {
     console.log("SegmentView")
->>>>>>> verlet
     this.model = model;
     this.context = context;
     game.on('render', this.render, this);
@@ -115,12 +23,7 @@ SegmentView = function(model, context) {
 
 SegmentView.prototype = {
     
-<<<<<<< HEAD
-    render: function() {
-        
-=======
     render: function() {        
->>>>>>> verlet
         this.context.fillStyle = this.model.colour;
         
         this.context.save();
@@ -132,8 +35,6 @@ SegmentView.prototype = {
         
         this.context.restore();
     }
-<<<<<<< HEAD
-=======
 }
 
 /*
@@ -161,5 +62,4 @@ SegmentController.prototype = {
                 
         this.model.lastPosition = this.model.position.copy();
     }
->>>>>>> verlet
 }
